c681b9099f424a3849944c552bd25ea1131eca9b
jiajinho <=>
2022-09-26 01:40:34 +0800

    Changed mongodb url to cloud atlas

--- config.ts
+++ config.ts
@@ -1,6 +1,7 @@
 export default {
   mongo: {
-    url: 'mongodb://127.0.0.1:27017',
+    // url: 'mongodb://127.0.0.1:27017',
+    url: 'mongodb+srv://admin:passw0rd@cluster0.hrjyfjo.mongodb.net/test',
     dbName: 'mighty-jaxx'
   },
   express: {
