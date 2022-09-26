f3a0f2825f019a66d324f820097f998bb4a9791f
jiajinho <hojiajin7@gmail.com>
2022-09-26 11:03:34 +0800

    Removed logs

--- index.ts
+++ index.ts
@@ -6,13 +6,8 @@ import { Mongo } from "./src/Mongo";
 
 if (process.env.NODE_ENV === "dev") dotenv({ path: `.env.${process.env.NODE_ENV}` });
 
-console.log("what seems to be problem");
-
 if (!process.env.JWT_SECRET) throw Error("JWT_SECRET not set");
 if (!process.env.MONGO_URL) throw Error("MONGO_URL not set");
-
-console.log("what seems to be problem2");
-
 
 const mongo = new Mongo();
 const express = new Express(mongo);
